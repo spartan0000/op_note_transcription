28902c2dba7276bb4594101d87336bee0d85efe5
David Lee <doctor.dlee@gmail.com>
2026-05-23 20:32:27 +1200

    add model validator

--- app/pydantic/note.py
+++ app/pydantic/note.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field, field_validator, model_validator
 from typing import Optional, List, Literal
 from datetime import datetime, date
 
@@ -12,6 +12,7 @@ class Note(BaseModel):
     date_of_procedure: Optional[date] = Field(default = None, description="Date of the procedure")
 
     @field_validator('date_of_dictation', 'date_of_procedure')
+    @classmethod
     def validate_dates(cls, v):
         if v is None:
             return None
@@ -19,6 +20,17 @@ class Note(BaseModel):
             raise ValueError("Dates cannot be in the future")
         if v and v < date(2000, 1, 1):
             raise ValueError("Dates cannot be before January 1, 2000")
+        return v
+    @model_validator(mode = 'after')
+    def validate_date_order(self):
+        if (
+            self.date_of_dictation and self.date_of_procedure and self.date_of_dictation < self.date_of_procedure
+        ):
+            self.date_of_dictation = None
+            self.date_of_procedure = None
+        return self
+
+        
     procedures: List[Optional[str]] = Field(default = None, description="The name of the procedure")
     procedure_description: Optional[str] = Field(default = None, description="Description of the operative procedure")
     ebl: Optional[float] = Field(default = None, description="The estimated blood loss")
